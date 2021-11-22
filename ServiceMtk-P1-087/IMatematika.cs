d866f5b251f6e579b65f2a1c16c5c0d1ebb7bc8b
DESKTOP-ND6HMKD\Fanky <andryfanky3@gmail.com>
2021-11-22 16:12:40 +0700

    Add MathFault and try catch

--- ServiceMtk-P1-087/IMatematika.cs
+++ ServiceMtk-P1-087/IMatematika.cs
@@ -12,15 +12,24 @@ namespace ServiceMtk_P1_087
     public interface IMatematika
     {
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         /* output->*/
         int Tambah(int a, int b); //method
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kurang(int a, int b); //input
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kali(int a, int b);
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Bagi(int a, int b);
+
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
     }
 
@@ -40,6 +49,15 @@ namespace ServiceMtk_P1_087
             get { return _y; }
             set { _y = value; }
         }
+    }
+
+    [DataContract]
+    class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
     }
 
 }
